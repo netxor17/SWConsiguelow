e5251c0774608968d0510042a405bfc574c0c2b3
netxor17 <61363012+netxor17@users.noreply.github.com>
2020-04-01 22:58:52 +0200

    actualizado
    Ya muestra los productos que hay en la base de datos, pero aun no inserta correctamente.

--- mostarProducto.php
+++ mostarProducto.php
@@ -1,14 +1,15 @@
 
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>"Mostrando productos"</title>
+        <title>Mostrar tabla productos</title>
     </head>
     <body>
         <centre>
-        <table style="width:100%">
+        <table>
          <thead>
-             <tr>
+            <tr>
                  <th>Imagen</th>
                  <th>Nombre</th>
                  <th>Descripcion</th>
@@ -17,31 +18,31 @@
                  <th>Talla</th>
                  <th>Color</th>
                  <th>Categoria</th>
-             </tr>
+            </tr>
+
          </thead>
             <tbody>
             <?php
-            include('login.php');
+            $conexion = new mysqli("localhost", "root", "", "tiendaonline");
                 $query= "SELECT * FROM productos"; //hago un query y muestro todas las filas de la tabla 'productos'
                 $resultado= $conexion ->query($query);
          while($fila=$resultado->fetch_assoc()){ //mientras se haya podido recoger una fila de la tabla 'productos' de la bd
             ?>
-    <tr>
-        <td><img src="data:image/png;base64,<?php echo base64_encode($fila['imagen']); ?>"/></td> <!-- muestro la imagen-->
+        <tr>
+        <td><img height="50px" src="data:image/jpeg;base64,<?php echo base64_encode($fila['imagen']); ?>"/></td> <!-- muestro la imagen-->
         <td><?php echo $fila['nombre']; ?></td>
         <td><?php echo $fila['descripcion']; ?></td>
         <td><?php echo $fila['precio']; ?></td>
         <td><?php echo $fila['talla']; ?></td>
         <td><?php echo $fila['color']; ?></td>
         <td><?php echo $fila['categoria']; ?></td>
-       
-    </tr>
+        </tr>
 
-    <?php
+        <?php
         } //fin del while
-    ?>
+        ?>
 
-            </tbody>
+        </tbody>
         </table>
         </centre>
     </body>
--- vender.php
+++ vender.php
@@ -1,28 +1,28 @@
 <?php
-include('login.php');
+//include('login.php');
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="estilo.css" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title>Vender producto</title>
     </head>
     <body>
         <centre>
-        <form action="anadir.php" method="POST" enctype="multipart/form-data">   <!-- formulario para rellenar los campos -->
+            <form action="anadir.php" method="POST" enctype="multipart/form-data">   <!-- formulario para rellenar los campos -->
             <input type="text" REQUIRED name="nombreProducto" placeholder="Nombre del producto.." value="" /></br></br>  <!-- REQUIRED significa campo obligatorio -->
             <input type="text" REQUIRED name="descripcion" placeholder="Descripcion producto.." value="" /></br></br>
             <input type="text" REQUIRED name="precio" placeholder="Precio producto.." value="" /></br></br>
-            <input type="text" REQUIRED name="unidades" placeholder="Descripcion producto.." value="" /></br></br>
-            <input type="text" REQUIRED name="talla" placeholder="Descripcion producto.." value="" /></br></br>
-            <input type="text" REQUIRED name="color" placeholder="Descripcion producto.." value="" /></br></br>
-            <input type="text" REQUIRED name="categoria" placeholder="Descripcion producto.." value="" /></br></br>
+            <input type="text" REQUIRED name="unidades" placeholder="Unidades.." value="" /></br></br>
+            <input type="text" REQUIRED name="talla" placeholder="Talla.." value="" /></br></br>
+            <input type="text" REQUIRED name="color" placeholder="Color..." value="" /></br></br>
+            <input type="text" REQUIRED name="categoria" placeholder="Categoria.." value="" /></br></br>
             <input type="file" name="imagen" /></br></br>
             <input type="submit" name="enviar" value="Enviar" /></br></br>
             <input type="reset" name="Borrar" value="Borrar formulario" /></p></br>
-        </form>
+            </form>
         </centre>
     </body>
 </html>
