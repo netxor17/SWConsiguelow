b76259e5cab8ec8cbc2072c53d92186ee6ae8180
DonSteve <aabad01@ucm.es>
2020-05-01 18:36:44 +0200

    Update vender.php

--- vender.php
+++ vender.php
@@ -5,9 +5,11 @@ use es\fdi\ucm\aw\FormularioVender;
 
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="styles/style.css" />
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Subir un producto</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Vender</title>
+    <link rel="stylesheet" href="styles/style.css">
     </head>
 
     <body>
